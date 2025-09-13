157ae710479e135293878961a04c06adf9a95d6e
pavani <bokkasampavanipavi201@gmail.com>
2025-09-13 11:28:40 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,18 @@
+class A
+{
+int a,b,c;
+public static void main(String args[])
+{
+a=10;b=20;
+c=a+b;
+{
+System.out.println("sum of two number:",+c);
+}
+void sum()
+a=200,b=100;
+c=a+b;
+{
+System.out.println("sum of two number:",+c);
+}
+}
+}
